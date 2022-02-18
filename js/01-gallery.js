37f99b39d4de7c1537828b06d103f80c9fec1394
bezyn4uk <bezyn4uk@gmail.com>
2022-02-18 23:03:01 +0200

    checking

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,4 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems[0]);
-``
+
+const galleryRef = document.querySelector('.gallery');
+
+const createGalleryItem = ({ preview, original, description }) => {
+  const emptyDiv = document.createElement('div');
+
+  const galleryItem = `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div>`;
+};
