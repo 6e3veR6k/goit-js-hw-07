2238ddcf8acaee9a20570850bd869d05bd284799
bezyn4uk <bezyn4uk@gmail.com>
2022-02-22 22:33:58 +0200

    finished second part

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+let instance;
+
+// creates img container
+const createGalleryItem = ({ preview, original, description }) => {
+  const resultLink = document.createElement('a');
+  resultLink.classList.add('gallery__item');
+  resultLink.href = original;
+
+  const galleryItem = `<img class="gallery__image" src="${preview}" alt="${description}"/>`;
+  resultLink.insertAdjacentHTML('afterbegin', galleryItem);
+  return resultLink;
+};
+
+// render all galleries items
+galleryRef.append(...galleryItems.map((i) => createGalleryItem(i)));
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  overlayOpacity: 1,
+  captions: true,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
