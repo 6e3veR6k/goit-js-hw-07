35484f5112c8b99257e7dbf102132c0f739259ff
bezyn4uk <bezyn4uk@gmail.com>
2022-02-22 21:55:31 +0200

    finished first part

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,9 @@
 import { galleryItems } from './gallery-items.js';
 
-// Change code below this line
-
 const galleryRef = document.querySelector('.gallery');
+let instance;
 
+// creates img container
 const createGalleryItem = ({ preview, original, description }) => {
   const resultDiv = document.createElement('div');
   resultDiv.classList.add('gallery__item');
@@ -19,7 +19,39 @@ const createGalleryItem = ({ preview, original, description }) => {
   return resultDiv;
 };
 
+// render all galleries items
 galleryRef.append(...galleryItems.map((i) => createGalleryItem(i)));
 
-const instance = basicLightbox.create(galleryRef);
-instance.show(() => console.log('lightbox now visible'));
+galleryRef.addEventListener('click', onImgClick);
+
+// handle img click
+function onImgClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  const html = getImgHtml(e);
+  instance = createModalInstance(html);
+  instance.show();
+}
+
+// create markup based on img attributes from event.target
+function getImgHtml(e) {
+  const imgLink = e.target.dataset.source;
+  const imgAlt = e.target.alt;
+  return `<img src="${imgLink}" alt="${imgAlt}" width="1280">`;
+}
+
+// creates modal
+function createModalInstance(html) {
+  return basicLightbox.create(html, {
+    onShow: () => window.addEventListener('keydown', onKeyPressed),
+    onClose: () => window.removeEventListener('keydown', onKeyPressed),
+  });
+}
+
+function onKeyPressed(e) {
+  if (e.code === 'Escape') {
+    instance.close();
+  }
+}
